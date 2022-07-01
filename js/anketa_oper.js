2cad57c90d92802a8e2579358c9a4a8f845fb523
anna <test@mail.ru>
2022-07-01 14:07:29 +0300

    anketa_oper

---
+++ js/anketa_oper.js
@@ -0,0 +1,15 @@
+const lastname = prompt('Ваша фамилия?');
+const firstname = prompt('Ваше имя?');
+const patronymic = prompt('Ваше отчество?');
+const age = Number(prompt('Ваш возраст?'));
+const gender = confirm('Ваш пол-мужской?');
+
+
+ var t=(age == 20 )? 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
