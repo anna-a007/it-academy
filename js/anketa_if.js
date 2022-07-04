1e64c32b8b6268795fc9a7002e18b25f2ebe5684
anna <test@mail.ru>
2022-07-04 13:20:38 +0300

    create ankets

--- js/anketa_if.js
+++ js/anketa_if.js
@@ -2,16 +2,30 @@ const lastname = prompt('Ваша фамилия?');
 const firstname = prompt('Ваше имя?');
 const patronymic = prompt('Ваше отчество?');
 const age = Number(prompt('Ваш возраст?'));
-const gender = confirm('Ваш пол-мужской?');
+let gender = confirm('Ваш пол - мужской?');
 
-
-if (age == 20 ) {
- 
-    alert('Ваш ФИО:'+ ' ' + lastname + firstname + patronymic +'\n' + 
-          'Ваш возраст в годах:'+ ' ' + age + '\n' + 
-          'Ваш возраст в днях:' + ' ' + (age * 365) + '\n' + 
-          'Через 5 лет вам будет:' + ' ' + (age + 5) + '\n' +
-          'Ваш пол:' + ' мужской' + '\n' +
-          'Вы на пенсии:' + ' ' + 'нет'
-        );
+if (gender) {
+    gender = 'мужской';
 }
+else {
+    gender = 'женский';
+}
+
+let pension;
+
+if (age >= 60) {
+    pension = 'да';
+}
+else {
+    pension = 'нет';
+}
+
+alert(
+    'Ваш ФИО:' + ' ' + lastname + ' ' + firstname + ' ' + patronymic + '\n' +
+    'Ваш возраст в годах:' + ' ' + age + '\n' +
+    'Ваш возраст в днях:' + ' ' + (age * 365) + '\n' +
+    'Через 5 лет вам будет:' + ' ' + (age + 5) + '\n' +
+    'Ваш пол:' + ' ' + gender + '\n' +
+    'Вы на пенсии:' + ' ' + pension + '\n'
+);
+
--- js/anketa_oper.js
+++ js/anketa_oper.js
@@ -2,14 +2,17 @@ const lastname = prompt('Ваша фамилия?');
 const firstname = prompt('Ваше имя?');
 const patronymic = prompt('Ваше отчество?');
 const age = Number(prompt('Ваш возраст?'));
-const gender = confirm('Ваш пол-мужской?');
+let gender = confirm('Ваш пол - мужской?');
+
+gender = (gender) ? 'мужской' : 'женский';
+let pension = (age >= 60) ? 'да' : 'нет';
 
 
- var t=(age == 20 )? 
- 
- 
- 
- 
- 
- 
- 
+alert(
+    'Ваш ФИО:' + ' ' + lastname + ' ' + firstname + ' ' + patronymic + '\n' +
+    'Ваш возраст в годах:' + ' ' + age + '\n' +
+    'Ваш возраст в днях:' + ' ' + (age * 365) + '\n' +
+    'Через 5 лет вам будет:' + ' ' + (age + 5) + '\n' +
+    'Ваш пол:' + ' ' + gender + '\n' +
+    'Вы на пенсии:' + ' ' + pension + '\n'
+);
